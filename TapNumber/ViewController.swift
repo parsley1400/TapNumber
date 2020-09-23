31b8e78c2196cae5362c1c6a28ac0af2413240df
parsley1400 <topthecap@gmail.com>
2020-09-23 22:06:23 +0900

    Initial Commit

---
+++ TapNumber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapNumber
+//
+//  Created by risako takeya on 2020/09/23.
+//  Copyright © 2020 Paseri Takeya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
